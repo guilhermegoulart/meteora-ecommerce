20e931ffd36b70407cc972fe2214f435e22b4153
Guilherme Goulart <guilhermegoulartmagela@gmail.com>
2024-04-25 11:50:46 -0300

    usando api route e buildando o projeto

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,19 @@ import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
 import { getCategorias, getTodosProdutos } from "../lib/api";
 
+async function fetchProdutosApi() {
+  const res = await fetch("http://localhost:3000/api/produtos");
+  if (!res.ok) {
+    throw new Error("Falha ao buscar produtos");
+  }
+
+  const produtos = await res.json();
+
+  return produtos;
+} 
+
 export default async function Home() {
-  const produtos = getTodosProdutos();
+  const { produtos } = await fetchProdutosApi();
   const categorias = getCategorias();
 
   return (
